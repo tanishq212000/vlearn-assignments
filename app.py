e9131baf519efe3ac46c3fbbab0bb733bf68c395
tanishq212000 <tanishqb212000@gmail.com>
2026-05-10 04:09:34 +0530

    Commit #2

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ def hello_world():
     return 'Hello, World!'
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
+
+#addiquickly add a new route to the app
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello, {name}!'
